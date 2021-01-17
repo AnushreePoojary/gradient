1e809e69b4ac50d38c70e2c7024ed098292f4df1
AnushreePoojary <75212317+AnushreePoojary@users.noreply.github.com>
2021-01-17 11:45:33 +0530

    Add files via upload

---
+++ src/main/java/com/anushree/gradient/MainActivity.java
@@ -0,0 +1,24 @@
+package com.anushree.gradient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.media.audiofx.DynamicsProcessing;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import javax.security.auth.login.LoginException;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
